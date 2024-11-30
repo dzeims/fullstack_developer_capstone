92bc45f09048f38844ee3b70fc4568e602ba121b
Guntis <gb322400@gmail.com>
2024-11-30 14:06:45 -0500

    Half compliting module 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -92,12 +92,12 @@ def get_cars(request):
 
 # # Update the `get_dealerships` view to render the index page with
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if state == "All":
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
@@ -115,12 +115,12 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        return JsonResponse({"status": 200, "dealer": dealership})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `add_review` view to submit a review
 def add_review(request):
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,17 @@
-import LoginPanel from "./components/Login/Login"
-import RegisterPanel from "./components/Register/Register"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import RegisterPanel from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
+//import Dealer from "./components/Dealers/Dealer";
+//import PostReview from "./components/Dealers/PostReview";
+//import SearchCars from "./components/Dealers/SearchCars";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers />} />
     </Routes>
   );
 }
